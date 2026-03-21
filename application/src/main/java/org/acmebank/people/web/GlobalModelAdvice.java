cd107e12fbdf1a23ed13cb7d72101ade4f90bbb1
Pete Suggitt <me@suggs.org.uk>
2026-03-21 17:45:10 +0000

    failing tests

--- application/src/main/java/org/acmebank/people/web/GlobalModelAdvice.java
+++ application/src/main/java/org/acmebank/people/web/GlobalModelAdvice.java
@@ -1,14 +1,30 @@
 package org.acmebank.people.web;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.Optional;
 
 @ControllerAdvice
 public class GlobalModelAdvice {
 
+    private final BuildProperties buildProperties;
+
+    @Autowired
+    public GlobalModelAdvice(Optional<BuildProperties> buildProperties) {
+        this.buildProperties = buildProperties.orElse(null);
+    }
+
     @ModelAttribute("requestURI")
     public String requestURI(final HttpServletRequest request) {
         return request.getRequestURI();
+    }
+
+    @ModelAttribute("appVersion")
+    public String appVersion() {
+        return buildProperties != null ? buildProperties.getVersion() : "dev-local";
     }
 }
